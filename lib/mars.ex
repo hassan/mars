4faaa1aaef756e0073ada231e7248b22bd04ad77
Hassan Schroeder <hassan@webtuitive.com>
2021-05-05 14:21:37 -0700

    Update versions, clean up

--- lib/mars.ex
+++ lib/mars.ex
@@ -1,18 +1,3 @@
 defmodule Mars do
-  @moduledoc """
-  Documentation for Mars.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mars.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
